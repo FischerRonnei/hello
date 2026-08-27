edd7cfda8d856c9e2b0e8fd8a66ceda5bc6469e0
Fischer Ronnei <fronnei2@huskers.unl.edu>
2026-08-27 08:57:19 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Fischer Ronnei
+ *         90144513@nebraska.edu
+ * Date: 08/25/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Fischer Ronnei\n");
+
+
+  return 0;
+}
